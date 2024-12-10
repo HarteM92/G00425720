840352467e898fca235f4c9caba33df2e1fcf681
HarteM92 <G00425720@atu.ie>
2024-12-10 15:35:21 +0000

    commit: Initial set up HomePage and Search

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular'; 
+import { FormsModule } from '@angular/forms'; 
+import { RouterModule } from '@angular/router'; 
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+  standalone: true, 
+  imports: [IonicModule, FormsModule, RouterModule], 
 })
 export class HomePage {
+  searchTerm: string = ''; 
+
   constructor() {}
+
+  search() {
+    console.log('Search term:', this.searchTerm);
+  }
 }
